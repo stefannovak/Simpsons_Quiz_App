6ceda34c96f7d446f24d7b357b435454b593ab62
Stefan Novak <stefannovak96@gmail.com>
2020-06-08 15:34:49 +0100

    Improved the questions implementation, and solved the answers Issue.
    Created a new class called Questions, which takes an ID, Question, 3 optional answers and an answer.
    Added 11 questions to a question list.
    
    TODOs:
    Improve the ID numbers. They're currently hard coded values. Should be automatically incremented.
    Add the values to the questiontext and buttontext values

--- app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainActivity.kt
+++ app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainActivity.kt
@@ -9,20 +9,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("Question #1: ${questions[1]}") //Testing
-        println(questions.toString()) //Testing
+        //Add questions to the question list
+        questionList.add(question1)
+        questionList.add(question2)
+        questionList.add(question3)
+        questionList.add(question4)
+        questionList.add(question5)
+        questionList.add(question6)
+        questionList.add(question7)
+        questionList.add(question8)
+        questionList.add(question9)
+        questionList.add(question10)
+        questionList.add(question11)
     }
 
-    val questions = listOf(
-        "What colour was Chief Wiggum's hair originally?",
-        "How many members are in the main Simpsons family?",
-        "Marge's sisters are Patty and ?",
-        "In what year did the Simpsons first season premiere?",
-        "Which was the first Sideshow Bob episode?",
-        "What football team is gifted to Homer by Hank Scorpio?",
-        "The towns baseball team are called the Springfield ?",
-        "Who founded the town of Springfield?",
-        "How old are Bart and Lisa?",
-        "What is the name of the clown on Channel 6?",
-        "Which crank call of Bart's actually answers the phone?")
+    var questionList = mutableListOf<Questions>()
+
+    val question1 = Questions(1, "What colour was Chief Wiggum's hair originally?",
+        "Blue", "Grey", "Green", "Black")
+    val question2 = Questions(2, "How many members are in the main Simpsons family?",
+        "6","4", "3","5")
+    val question3 = Questions(3, "Marge's sisters are Patty and ?",
+        "Edna", "Maggie", "Margeret", "Selma")
+    val question4 = Questions(4, "In what year did the Simpsons first season premiere?",
+        "1990","1991","1992","1989")
+    val question5 = Questions(5, "Which was the first Sideshow Bob centered episode?",
+        "The Telltale Head", "Cape Feare", "Sideshow Bob's Last Gleaming", "Krusty Gets Busted")
+    val question6 = Questions(6, "What football team is gifted to Homer by Hank Scorpio?",
+        "Dallas Cowboys", "Green Bay Packers", "New England Patriots", "Denver Broncos")
+    val question7 = Questions(7, "The towns baseball team are called the Springfield ?",
+        "Dodgers", "Nucleus","Jets", "Isotopes")
+    val question8 = Questions(8, "Who founded the town of Springfield?",
+        "Abraham Simpson", "Springfield Manhatten", "Cornelius Quimby", "Jebadiah Springfield")
+    val question9 = Questions(9, "How old are Bart and Lisa?",
+        "9 and 10", "10 and 12", "7 and 9", "10 and 8")
+    val question10 = Questions(10, "What is the name of the clown on Channel 6?",
+        "Handsome Pete", "Krumpet the Klown", "Gabbo", "Krusty the Clown")
+    val question11 = Questions(11, "Which crank call of Bart's actually answers the phone?",
+        "Amada Huggenkiss", "B.O Problem", "I.P Freely", "Hugh Jazz")
+
 }
---
+++ app/src/main/java/com/stefanalexnovak/simpsonsquizapp/Questions.kt
@@ -0,0 +1,11 @@
+package com.stefanalexnovak.simpsonsquizapp
+
+public class Questions(questionId: Int,
+                       question: String,
+                       optionalAnswerA: String,
+                       optionalAnswerB: String,
+                       optionalAnswerC: String,
+                       questionAnswer: String) {
+
+
+}
