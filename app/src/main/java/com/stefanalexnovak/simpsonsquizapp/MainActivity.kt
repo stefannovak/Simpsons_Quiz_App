9f838b5b362d59c9dc7fc3d5ea7608444e6edb09
Stefan Novak <stefannovak96@gmail.com>
2020-06-08 14:29:15 +0100

    Implemented the questions as a List in the MainActivity class

--- app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainActivity.kt
+++ app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainActivity.kt
@@ -8,5 +8,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Question #1: ${questions[1]}") //Testing
+        println(questions.toString()) //Testing
     }
+
+    val questions = listOf(
+        "What colour was Chief Wiggum's hair originally?",
+        "How many members are in the main Simpsons family?",
+        "Marge's sisters are Patty and ?",
+        "In what year did the Simpsons first season premiere?",
+        "Which was the first Sideshow Bob episode?",
+        "What football team is gifted to Homer by Hank Scorpio?",
+        "The towns baseball team are called the Springfield ?",
+        "Who founded the town of Springfield?",
+        "How old are Bart and Lisa?",
+        "What is the name of the clown on Channel 6?",
+        "Which crank call of Bart's actually answers the phone?")
 }
