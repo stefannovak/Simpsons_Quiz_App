65ca249727f4eebfc359649e63af5e4203588352
Stefan Novak <stefannovak96@gmail.com>
2020-06-15 14:45:02 +0100

    Created an about popup. No hyperlinks
    closes #29
    
    Signed-off-by: Stefan Novak <stefannovak96@gmail.com>

--- app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainMenuActivity.kt
+++ app/src/main/java/com/stefanalexnovak/simpsonsquizapp/MainMenuActivity.kt
@@ -3,14 +3,11 @@ package com.stefanalexnovak.simpsonsquizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.util.Linkify
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.main_menu.*
 
 class MainMenuActivity : AppCompatActivity() {
-
-    companion object {
-        const val INTENT_KEY = "quiz"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
@@ -19,11 +16,24 @@ class MainMenuActivity : AppCompatActivity() {
         playButton.setOnClickListener{
             goToQuiz()
         }
+
+        aboutButton.setOnClickListener{
+            aboutDialog()
+        }
     }
 
     private fun goToQuiz() {
         val quiz = Intent(this, MainActivity::class.java)
-//        quiz.putExtra(INTENT_KEY)
         startActivity(quiz)
+    }
+
+    private fun aboutDialog() {
+        val alertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setMessage("Hi! I hope you're enjoying this little app. This was the first app I created.\n\n" +
+                "If you're interested in getting to know me or contacting me, here is a list of my socials:\n\n" +
+                "Github\n\n" +
+                "LinkedIn\n\n" +
+                "Twitter")
+        alertDialog.show()
     }
 }
